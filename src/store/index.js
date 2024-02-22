702b9c2885e2e620550e8922faa66a00c83e9587
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2024-02-22 17:04:33 +0100

    .....

--- src/store/index.js
+++ src/store/index.js
@@ -9,11 +9,11 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
 state: {
     blogPosts: [
-      {
-        blogTitle:"Blog Card #1", blogCoverPhoto:"stock-1", blogDate:"May 1, 2021" },    {
-        blogTitle:"Blog Card #2", blogCoverPhoto:"stock-2", blogDate:"May 1, 2021" },    {
-        blogTitle:"Blog Card #3", blogCoverPhoto:"stock-3", blogDate:"May 1, 2021" },    {
-        blogTitle:"Blog Card #4", blogCoverPhoto:"stock-4", blogDate:"May 1, 2021" }
+      // {
+      //   blogTitle:"Blog Card #1", blogCoverPhoto:"stock-1", blogDate:"May 1, 2021" },    {
+      //   blogTitle:"Blog Card #2", blogCoverPhoto:"stock-2", blogDate:"May 1, 2021" },    {
+      //   blogTitle:"Blog Card #3", blogCoverPhoto:"stock-3", blogDate:"May 1, 2021" },    {
+      //   blogTitle:"Blog Card #4", blogCoverPhoto:"stock-4", blogDate:"May 1, 2021" }
     ],
     postLoaded: null,
     blogHTML: "Write your blog title here...",
